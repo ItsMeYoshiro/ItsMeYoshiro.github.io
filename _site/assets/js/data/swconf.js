cecb64bde44916d52f696f3e38567ca52c320e9d
Kaio Barbosa <92401977+ItsMeYoshiro@users.noreply.github.com>
2025-04-21 13:55:27 -0300

    Detalhes2

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1745254268',resources: [
+    cacheName: 'chirpy-1745254477',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
