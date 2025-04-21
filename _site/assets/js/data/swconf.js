30ca5139436f467420b2ee6e5f4433e514871469
Kaio Barbosa <92401977+ItsMeYoshiro@users.noreply.github.com>
2025-04-21 09:54:34 -0300

    NovoPortfolio

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1736759466',resources: [
+    cacheName: 'chirpy-1745240021',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
